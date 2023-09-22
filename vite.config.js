bd66a82182a9197bd7c76a79922f52a0d26cdbaf
Sathish Projects <kalivimarsa705@gmail.com>
2023-09-22 08:44:31 +0530

    add:deploy commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/sathish-1023/my3d_portfolio.git",
+  base: "/myPortfolio/",
   plugins: [react()],
 })
